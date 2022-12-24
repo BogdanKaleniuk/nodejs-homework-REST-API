074d6a16f9ce51cf707b44b5343901ca4f7060a8
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-12-24 20:40:54 +0200

    //

--- db/app.js
+++ db/app.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const dotevn = require("dotenv");
+
+dotevn.config();
 
 console.log(process.env);
 
 const { DB_HOST } = process.env;
-
+console.log(DB_HOST);
 mongoose
   .connect(DB_HOST)
   .then(() => console.log("Database connection successful"))
