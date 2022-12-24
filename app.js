664eb6a69b5bdf5d6872c915f7b3731fa3e97382
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-12-24 19:42:48 +0200

    add env

--- app.js
+++ app.js
@@ -24,4 +24,4 @@ app.use((err, req, res, next) => {
 
 module.exports = app;
 
-// CeNDXYgpllEv2dRA;
+// dMATlKVAmXztunH7;
---
+++ db/app.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const { DB_HOST } = require("./config");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
