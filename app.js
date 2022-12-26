fa82ef79e40d2c203e44f6bd066ff25e9f10284d
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-12-26 04:54:18 +0200

    done

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+// Web-Server (express)
+// 1.Middleware
+// 2. Тоді маршрути
+// 3. В кінці обробка помилок
 const express = require("express");
-const logger = require("morgan");
+const logger = require("morgan"); // виводить в консоль інфо про запити
 const cors = require("cors");
-require("dotenv").config();
+
+// require('dotenv').config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const contactsRouter = require("./routes/api/contacts");
 
@@ -9,20 +16,20 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger));
+app.use(logger(formatsLogger)); // middleware виводить дані (запити)
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // перевіряє Content-Type,тіло парсить як обєкт
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter); // якщо запит поч. з 'api/contacts', шукай обробників маршрутів тут contactsRouter
 
+// обробка помилок
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 module.exports = app;
-
-// dMATlKVAmXztunH7;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,117 +1,18 @@
 const express = require("express");
-const router = express.Router();
-const { NotFound } = require("http-errors");
-const Joi = require("joi");
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../../models/contacts");
+const ctrl = require("../../controllers/contacts"); 
+const router = express.Router(); 
+const { ctrlWrapper } = require("../../helpers");
 
-const contactsSchema = Joi.object({
-  name: Joi.string().trim().required(),
-  email: Joi.string().email().trim().required(),
-  phone: Joi.string().required(),
-});
+router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result: contacts,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", ctrlWrapper(ctrl.getById));
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await getContactById(id);
-    if (!result) {
-      throw new NotFound(`Contact with id=${id} not found`);
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", ctrlWrapper(ctrl.add));
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = contactsSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
-    const result = await addContact(req.body);
-    res.status(201).json({
-      status: "success",
-      code: 201,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:id", ctrlWrapper(ctrl.updateById));
 
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { error } = contactsSchema.validate(req.body);
-    if (error) {
-      error.status = 400;
-      throw error;
-    }
-    const { id } = req.params;
-    const result = await updateContact(id, req.body);
-    if (!result) {
-      throw new NotFound(`Contact with id=${id} not found`);
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:id", ctrlWrapper(ctrl.removeById));
 
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await removeContact(id);
-    if (!result) {
-      throw new NotFound(`Contact with id=${id} not found`);
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      message: "contact delete",
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch("/:id/favorite", ctrlWrapper(ctrl.updateStatusContact));
 
 module.exports = router;
